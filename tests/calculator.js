4958d769142827235210adf5075d052eec017b0f
nishank <nishankbharil@yahoo.com>
2020-03-26 11:48:39 +0530

    First commit

---
+++ tests/calculator.js
@@ -0,0 +1,21 @@
+let homepage = require('../pages/homepage');
+
+describe('demo calculator tests', function () {
+
+    it('addition test', function () {
+
+        homepage.get('https://juliemr.github.io/protractor-demo/');
+
+        homepage.enterFirstNumber('4');
+
+        homepage.enterSecondNumber('3');
+
+        homepage.clickGo();
+
+        homepage.verifyResult('7');
+        browser.sleep(2000);
+
+
+    });
+
+});
